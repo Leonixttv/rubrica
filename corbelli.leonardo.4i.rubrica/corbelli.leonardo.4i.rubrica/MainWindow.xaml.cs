fe11cc89d2f9b06bfd9e5df04ec5af910c24b84a
Leonardo Corbelli <121753948+Leonixttv@users.noreply.github.com>
2023-10-04 08:22:42 +0200

    Add files via upload

---
+++ corbelli.leonardo.4i.rubrica/corbelli.leonardo.4i.rubrica/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace corbelli.leonardo._4i.rubrica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            contatto c = new contatto();
+            c.Numero = 1;
+            c.Nome = "leonardo";
+            c.Cognome = "corbelli";
+
+            contatto[] Contatti = new contatto[100];
+            Contatti[0] = c;
+
+            Contatti[0].Nome = "bho";
+            Contatti[0].Cognome = "non so";
+        }
+    }
+}
